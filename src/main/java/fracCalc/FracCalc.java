62f91ba7d29025052ceb973368c16cc67beb9964
s-kwonge <s-kwonge@53S50022879.it.bsd405.org>
2019-11-12 08:54:14 -0800

    checkpoint #1

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -4,10 +4,23 @@
 
 package fracCalc;
 
+import java.util.Scanner;
+
 public class FracCalc {
 
-    public static void main(String[] args)
+    public static void main(String[] args)//has a while loop and will print the produce Answer. 
+    
+    	
     {
+    	
+    	Scanner userInput = new Scanner (System.in);
+    	
+//    	System.out.print("What is your equation? : ");
+    	String stringInput =userInput.nextLine();
+    	
+    	System.out.println(produceAnswer(stringInput));                        
+    	
+    	userInput.close();
         // TODO: Read the input from the user and call produceAnswer with an equation
 
     }
@@ -22,9 +35,16 @@ public class FracCalc {
     //      e.g. return ==> "1_1/4"
     public static String produceAnswer(String input)
     {
+    	
+//    	System.out.println("Length of string = " + input.length());
+    	int postwo = input.indexOf(" ") + 3;
+//    	System.out.println("Position of second fraction = " + postwo);
+//    	System.out.println("");
+    	
+    	
         // TODO: Implement this function to produce the solution to the input
 
-        return "";
+        return input.substring(postwo);//returns what you want to output into the console
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
