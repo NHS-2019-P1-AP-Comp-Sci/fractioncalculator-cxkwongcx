7527a151e7ec73e1f270c5d2a66fee43f7cf6792
s-kwonge <s-kwonge@53S50022879.it.bsd405.org>
2019-11-22 23:50:40 -0800

    Part Two check point

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -8,22 +8,24 @@ import java.util.Scanner;
 
 public class FracCalc {
 
-    public static void main(String[] args)//has a while loop and will print the produce Answer. 
+    public static void main(String[] args){//has a while loop and will print the produce Answer. 
     
-    	
-    {
-    	
     	Scanner userInput = new Scanner (System.in);
-    	
+    	String stringInput = userInput.nextLine();
+    	boolean quitTest = stringInput.equals("quit");
+    	while(quitTest == false){
 //    	System.out.print("What is your equation? : ");
-    	String stringInput =userInput.nextLine();
+    	System.out.println(produceAnswer(stringInput));     
+    	stringInput= userInput.nextLine();
+    	if (stringInput == "quit") {
+    		quitTest = true;
+    	}
     	
-    	System.out.println(produceAnswer(stringInput));                        
+        // TODO: Read the input from the user and call produceAnswer with an equation
+    	}
     	
     	userInput.close();
-        // TODO: Read the input from the user and call produceAnswer with an equation
-
-    }
+    }	
 
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
     // This function takes a String 'input' and produces the result
@@ -33,20 +35,55 @@ public class FracCalc {
     //
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
+    public static String produceAnswer(String input){
+    	String whole = "0";
+    	String numerator = "0";
+    	String denominator = "0";
+
+    if (input.equals("quit"));{
     	
+    	//move back here
 //    	System.out.println("Length of string = " + input.length());
-    	int postwo = input.indexOf(" ") + 3;
-//    	System.out.println("Position of second fraction = " + postwo);
-//    	System.out.println("");
+    	int posTwo = input.indexOf(" ") + 3;                  //gives the position of the beginning of the part after _+_(second number)
+    	String testOne = input.substring(posTwo);             //prints out the second number from the position it was told it started
+    	if (testOne.indexOf("_") != -1) {
+    		int positionEndOne = testOne.indexOf("_");
+    		whole = testOne.substring(0,positionEndOne);
+    		
+    	}
     	
+    	
+    	if (testOne.indexOf("/") != -1) {
+    		int positionEndTwo = testOne.indexOf("/");
+    		denominator =  testOne.substring(positionEndTwo + 1);
+    		if (testOne.indexOf("_") != -1) {
+    			int positionOfUnderscorePlusOne = testOne.indexOf("_") + 1;
+//    			String stuffAfterUnderscore = testOne.substring(positionOfUnderscore+1);
+    			numerator =  testOne.substring(positionOfUnderscorePlusOne,positionEndTwo);
+    		}
+    		
+    		if(testOne.indexOf("_") == -1) {
+       			String testTwo = input.substring(posTwo - 1);
+       			int positionEndTwoNew = testTwo.indexOf("/");
+    			numerator = testTwo.substring(0,positionEndTwoNew);
+    		}
+    	}
+    		
+    	else if(testOne.indexOf("_") == -1 && testOne.indexOf("/") == -1){
+//    		String three = input.substring(posTwo -1);
+    		String onlyDenom = testOne;
+    		 whole = onlyDenom.substring(0);
+    		
+    	}   	
     	
         // TODO: Implement this function to produce the solution to the input
 
-        return input.substring(postwo);//returns what you want to output into the console
+        return ("whole:" + whole + " numerator:" + numerator + " denominator:" + denominator);//returns what you want to output into the console
+        
+        
+    	}
     }
-
+}
     // TODO: Fill in the space below with any helper methods that you think you will need
 
-}
+
