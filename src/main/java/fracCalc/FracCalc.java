b75959be24426b18eef87963bd8af7bdef266156
s-kwonge <s-kwonge@53S50022879.it.bsd405.org>
2019-12-07 22:23:01 -0800

    commit test

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -6,159 +6,152 @@ package fracCalc;
 
 import java.util.Scanner;
 
+
 public class FracCalc {
 
 	public static void main(String[] args) {// has a while loop and will print the produce Answer.
 
 		Scanner userInput = new Scanner(System.in);
 		String stringInput = userInput.nextLine(); // this is for the second number stuff
-		String firstInput = stringInput; // this is for the first number stuff
-		boolean quitTest = stringInput.equals("quit");
-		while (quitTest == false) {
-//    	System.out.print("What is your equation? : ");
-			System.out.println(inputs(stringInput, firstInput)); // (input for second, input for first to use)
+		while(!(stringInput == "quit")) {
+			System.out.println(produceAnswer(stringInput));
 			stringInput = userInput.nextLine();
-			if (stringInput == "quit") {
-				quitTest = true;
-			}
-
 		}
-
 		userInput.close();
+		
 	}
-
-	public static void AllNumbers() {
-
-	}
-
-	public static String inputs(String input, String input1) {
-		String whole1 = "0";
-		String num1 = "0";
-		String denom1 = "0";
+	
+	
+	
+	
+	
+	
+	public static String produceAnswer(String input) {
 		
-		int posOne = input.indexOf(" ");
+		int posOne = input.indexOf(" ");// index of the first space
+		String firstPart = input.substring(0, posOne);
+		String operator = input.substring(posOne+1 , posOne+2);
+		String secondPart = input.substring(posOne+3);
+//		^^This stuff above helps divide the equation into 3 parts,^^
+//		First Number, Operator, and Second Number.
+//  ==========================================================================================================		
+//		String wString1 = Whole(firstPart);
+//		String nString1 = Num(firstPart);
+//		String dString1 = Dem(firstPart);
+////		^^getting strings for the first number^^
+//		String wString2 = Whole(secondPart);
+//		String nString2 = Num(secondPart);
+//		String dString2 = Dem(secondPart);
+////		^^getting strings for the second number^^
+//  ==========================================================================================================
+		int intWhole1 = Integer.parseInt(Whole(firstPart));
+		int intWhole2 = Integer.parseInt(Whole(secondPart));
+		int intNum1 = Integer.parseInt(Num(firstPart));
+		int intNum2 = Integer.parseInt(Num(secondPart));
+		int intDenom1 = Integer.parseInt(Dem(firstPart));
+		int intDenom2 = Integer.parseInt(Dem(secondPart));
+//		changing my Strings of whole, numerators, and denominators into integers from strings
+//  ==========================================================================================================
+		int newNum = 0;
+		int newDenom = 0;
+//		^^setting up defaults
+//  ==========================================================================================================		
+		intNum1 = intNum1 + Math.abs(intWhole1) * intDenom1; //makes an improper fraction for first number
+		intNum2 = intNum2 + Math.abs(intWhole2) * intDenom2; //makes an improper fraction for second number
 		
-		
-		String beginning = input1.substring(0,posOne); //this is the string of stuff that comes before the first space
-		int firstUnderscore = beginning.indexOf("_");
-		int firstSlash = beginning.indexOf("/");
-//		int firstSpace = beginning.indexOf(" ");
-		if (beginning.indexOf("_") != -1) {
-			whole1 = beginning.substring(0, firstUnderscore);
+		if (intWhole1 <0) { //tests if the first number was a negative
+			intNum1 *= -1;
+		}
+		if (intWhole2 <0) { //tests if the second number as a negative
+			intNum2 *= -1;
 		}
 		
-		if (beginning.indexOf("/") != -1) {
-			denom1 = beginning.substring(firstSlash +1);
-			if (beginning.indexOf("_") != -1) {
-				num1 = beginning.substring(firstUnderscore+1, firstSlash);
-			}
-			if (beginning.indexOf("_") == -1) {
-				num1 = beginning.substring(0, firstSlash);
-			}
+		if (operator == "+") {// if operator is '+'
+			newNum = intNum1 * intDenom2 + intNum2 * intDenom1;
+			newDenom = intDenom1 * intDenom2;
+		}
+		if (operator == "-") {// if operator is '-'
+			newNum = intNum1 * intDenom2 - intNum2 * intDenom1;
+			newDenom = intDenom1 * intDenom2;
+		}
+		if (operator == "*") {// if operator is '*'
+			newNum = intNum1 * intNum2;
+			newDenom = intDenom1 * intDenom2;
+		}
+		if (operator == "/") {// if operator is '/'
+			newNum = intNum1 * intDenom2;
+			newDenom = intDenom1 * intNum2;
 			
 		}
-		else if (beginning.indexOf("_") == -1 && beginning.indexOf("/") == -1) {
-			whole1 = beginning.substring(0, posOne ); 
-			denom1 = "1";
-		}
-		//___________________________Below is for the second number
-		String whole2 = "0";
-		String num2 = "0";
-		String denom2 = "0";
-		// move back here
-//    	System.out.println("Length of string = " + input.length());
-		int posTwo = input.indexOf(" ") + 3; // gives the position of the beginning of the part after _+_(second number)
-		String testOne = input.substring(posTwo); // prints out the second number from the position it was told it
-													// started
-		if (testOne.indexOf("_") != -1) {
-			int positionEndOne = testOne.indexOf("_");
-			whole2 = testOne.substring(0, positionEndOne);
-		}
+		return newNum + "/" + newDenom;
+////		-----------------------------------------------------------------------------------------------------
+////		if the improper fraction is less than 1
+//		if ((newNum / newDenom) == 0) {
+//			return newNum + "/" + newDenom;
+//		}
+//		
+////		-----------------------------------------------------------------------------------------------------
+////		if the improper faction is bigger than 1
+//		else {
+//			finalWhole = newNum / newDenom;
+//			newNum = Math.abs(newNum) % newDenom;
+//			return finalWhole + "_" + newNum + "/" + newDenom;
+//		}
 
-		if (testOne.indexOf("/") != -1) {// finds if there is a fraction in the second number
-			int positionEndTwo = testOne.indexOf("/");
-			denom2 = testOne.substring(positionEndTwo + 1);
-			if (testOne.indexOf("_") != -1) {
-				int positionOfUnderscorePlusOne = testOne.indexOf("_") + 1;
-				num2 = testOne.substring(positionOfUnderscorePlusOne, positionEndTwo);
-			}
-
-			if (testOne.indexOf("_") == -1) {
-				String testTwo = input.substring(posTwo - 1);
-				int positionEndTwoNew = testTwo.indexOf("/");
-				num2 = testTwo.substring(0 + 1, positionEndTwoNew);
-			}
+	}
+	
+	
+	
+	
+	
+	
+	
+//	===========================================================================================================
+//	-------------start of Whole Parsing
+	public static String Whole(String number) {//Parsing out a whole number from the input
+		if (number.indexOf("_") != -1) {
+			return number.substring(0,number.indexOf("_"));
 		}
-
-		else if (testOne.indexOf("_") == -1 && testOne.indexOf("/") == -1) {
-			String onlyDenom = testOne;
-			whole2 = onlyDenom.substring(0);
-			denom2 = "1";
-
+		else if(number.indexOf("_") != -1 && number.indexOf("/") != -1 && number.length() != 0){
+			return "0";
 		}
-		String operator = input.substring(posOne, posTwo);
+		else {
+			return number;
+		}
+	}
+//	-------------end of Whole Parsing
+//	-------------start of Numerator Parsing
+	public static String Num(String number) {//Parsing out a Numerator number from the input
+		if (number.indexOf("_") != -1) {
+			return number.substring(number.indexOf("_") + 1, number.indexOf("/")); 
+		}
+		else if(number.indexOf("/") != -1) {
+			return number.substring(0, number.indexOf("/"));
+		}
+		else {
+			return "0";
+		}
 		
-		if (operator == "+") {
-			if (whole1.indexOf("-") == -1) {
-				return (add(whole1, whole2, num1, denom1, num2, denom2));
-			}
-		}
-
-		// TODO: Implement this function to produce the solution to the input
-
-		return ("second int stuff=:" + "      whole2:" + whole2 + "      numerator2:" + num2 + "      denominator2:" + denom2
-				 + "     first in stuff=" + "    whole1:" + whole1 + "      numerator1:" + num1 + "      denominator1:" + denom1);// returns what you want
-																								// to output into the
-																								// console
-
+		
 		
 	}
-
-//-----------------------------------------------------------------------------------------------------------------------------
-	public static String add(int whole1, int whole2, int num1, int denom1, int num2, int denom2) {
-		int num1fix = (whole1 * denom1) + num1;
-
-		int num2fix = (whole2 * denom2) + num2;
-
-		int newnum1 = num1fix * denom2;
-		int newnum2 = num2fix * denom1;
-		int newdenom = denom2 * denom1;
-
-		int completenum = newnum1 + newnum2;
-
-		return completenum + "/" + newdenom;
-
+//	-------------end of Numerator Parsing
+//	-------------start of Denominator Parsing
+	public static String Dem(String number) {//Parsing out a Denominator number from the input
+		if (number.indexOf("/") != -1) {
+			return number.substring(number.indexOf("/") + 1);
+		}
+		else {
+			return "1";
+		}
+		
 	}
+//	-------------end of Denominator Parsing
+//  ==========================================================================================================	
 
-//-----------------------------------------------------------------------------------------------------------------------------
-	public static String substract(int whole1, int whole2, int num1, int denom1, int num2, int denom2) {
-		int newnum1 = num1 * denom2;
-		int newnum2 = num2 * denom1;
-		int newdenom = denom2 * denom1;
-
-		int completenum = newnum1 - newnum2;
-
-		return completenum + "/" + newdenom;
-
-	}
-
-//-----------------------------------------------------------------------------------------------------------------------------
-	public static String multiply(int whole1, int whole2, int num1, int denom1, int num2, int denom2) {
-		int newnum = num1 * num2;
-		int newdenom = denom1 * denom2;
-
-		return newnum + "/" + newdenom;
-
-	}
-
-//-----------------------------------------------------------------------------------------------------------------------------
-	public static String devide(int whole1, int whole2, int num1, int denom1, int num2, int denom2) {
-		int newnum = num1 * denom2;
-		int newdenom = denom1 * num2;
-
-		return newnum + "/" + newdenom;
-	}
 
 }
-// TODO: Fill in the space below with any helper methods that you think you will
-// need
+
+//not working
+
+		
